cb806342245bd79f1c739f0c988b73ee91611389
kimchanho97 <nh0903@pusan.ac.kr>
2025-03-10 07:24:59 +0900

    feat: 채팅방 입장 바텀시트 구현

---
+++ src/components/portfoliodetail/ChatConfirmBottomSheet.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import CircularProgress from "@mui/material/CircularProgress";
+import BottomSheet from "../common/bottomsheet/BottomSheet";
+import { createChatRoom } from "../../apis/chat";
+import useOpenBottomSheet from "../../hooks/useOpenBottomSheet";
+import useDefaultErrorHandler from "../../hooks/useDefaultErrorHandler";
+
+export default function ChatConfirmBottomSheet({
+  plannerName,
+  plannerId,
+  onClose,
+}) {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const { isLogged } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const { defaultErrorHandler } = useDefaultErrorHandler();
+  const { openBottomSheetHandler } = useOpenBottomSheet();
+
+  const handleOnCreateChatRoom = async () => {
+    if (!isLogged) {
+      openBottomSheetHandler({ bottomSheet: "loginBottomSheet" });
+      return;
+    }
+    setIsSubmitting(true);
+    try {
+      const response = await createChatRoom(plannerId);
+      navigate(`/chat/${response.chatRoomId}`);
+    } catch (error) {
+      defaultErrorHandler(error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
+    <BottomSheet onClose={onClose}>
+      <div>
+        <div className="flex flex-col tracking-tight font-bold text-lg pb-10">
+          <span>웨딩 상담을 고민 중이신가요?</span>
+          <span>{plannerName} 플래너님과 편하게 대화해보세요!</span>
+        </div>
+        <button
+          className="block w-full h-[50px] rounded-[10px] text-sm bg-lightskyblue-sunsu"
+          onClick={handleOnCreateChatRoom}
+          disabled={isSubmitting}
+        >
+          {isSubmitting ? <CircularProgress size={20} /> : <span>입장</span>}
+        </button>
+      </div>
+    </BottomSheet>
+  );
+}
